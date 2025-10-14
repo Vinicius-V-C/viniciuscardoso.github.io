6b221050180e936a6b9385e26eadfbd9b2fa7526
viniciuscardoso <vivoco@icloud.com>
2025-10-14 07:28:38 +0000

    hfytcytfytf

--- lab5/main.js
+++ lab5/main.js
@@ -18,10 +18,40 @@ const inputColorir = document.querySelector("#escreve input");
 const cores = ['red','blue','green'];
 let interador = 0;
 
-
 function colorir(){
     inputColorir.style.background = cores[interador];
     interador = ++interador % cores.length;
 }
 
 inputColorir.onkeyup = colorir;
+
+mudarFundo = () => {
+  const cor = document.querySelector("#selecionaCor").value;
+  document.body.style.background = cor;
+};
+
+
+let contador = 0;
+
+function contar() {
+  contador++;
+  document.querySelector("#contador").textContent = contador;
+}
+
+function mostrarMensagem() {
+  const nome = document.querySelector("#nome").value;
+  const idade = document.querySelector("#idade").value;
+  const msg = document.querySelector("#mensagem");
+
+  msg.textContent = `Olá, o ${nome} tem ${idade}!`;
+}
+
+let contadorAuto = 0;
+
+function atualizarContador() {
+  contadorAuto++;
+  document.querySelector("#autoContador").textContent = contadorAuto;
+}
+
+// aumenta 1 a cada segundo (1000 milissegundos)
+setInterval(atualizarContador, 1000);
